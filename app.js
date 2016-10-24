89e7273397066aec93abf1545bcd658d1fa3e312
d0p3t <remcotroost@users.noreply.github.com>
2016-10-24 15:39:26 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ client.connect();
 
 
 client.on("subscription", function (channel, username, method) {
-    // Do your stuff.
 
     client.say(channel, "TwitchLit " + username + " has just subscribed to " + channel + "!!! TwitchLit");
+
 });
 
 client.on("resub", function (channel, username, months, message) {
-    // Do your stuff.
 
     client.say(channel, "TwitchLit " + username + " has just re-subscribed to " + channel + " for " + months +"!!! TwitchLit");
+    
 });
